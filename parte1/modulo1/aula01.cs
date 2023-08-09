b674bcc94d8693964e09d856edd28e4a81a62ebc
Diego <62783181+rabdiego@users.noreply.github.com>
2023-08-09 18:44:01 +0000

    questoes 2 e 3 feitas

--- parte1/modulo1/aula01.cs
+++ parte1/modulo1/aula01.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;  // Estrutura de lista
 
 class Questao1
 {
-    public static int IndiceMaximo(List <int> L_1, List <int> L_2, int inicio, int fim)
+    private static int IndiceMaximo(List <int> L_1, List <int> L_2, int inicio, int fim)
     {
         // Copiando as listas para não bagunçar ponteiros
         List <int> L_1_copy = new List <int> (L_1);
@@ -23,8 +23,13 @@ class Questao1
             fim_2 = fim - N;
         }
 
+        // Tudo em cima é O(1)*
+        // * Tecnicamente a função de copiar é O(n), mas para fins da disciplina, é possível assumir O(1)
+
+        // O(n)
         for (int i = inicio; i < fim_1; i++)
         {
+            // O(1)
             if (L_1_copy[i] > maior)
             {
                 maior = L_1_copy[i];
@@ -32,8 +37,10 @@ class Questao1
             }
         }
 
+        // O(n)
         for (int i = inicio; i < fim_2; i++)
         {
+            // O(1)
             if (L_2_copy[i] > maior)
             {
                 maior = L_2_copy[i];
@@ -41,10 +48,11 @@ class Questao1
             }
         }
 
+        // Total: O(n)
         return indiceMaior;
     }
 
-    public static (List <int>, List <int>) Troca(List <int> L_1, List <int> L_2, int a, int b)
+    private static (List <int>, List <int>) Troca(List <int> L_1, List <int> L_2, int a, int b)
     {
         List <int> L_1_copy = new List <int> (L_1);
         List <int> L_2_copy = new List <int> (L_2);
@@ -73,6 +81,8 @@ class Questao1
             L_2_copy[b - N] = temp;
         }
 
+        // Tudo é O(1)
+
         return (L_1_copy, L_2_copy);
     }
 
@@ -82,13 +92,143 @@ class Questao1
         List <int> L_2_copy = new List <int> (L_2);
         int index, N = L_1_copy.Count;
 
-        for (int i = 1; i < 2*N; i++)
+        // Tudo em cima é O(1)
+
+        for (int i = 1; i < 2*N; i++)  // O(n)
         {
-            index = IndiceMaximo(L_1_copy, L_2_copy, 0, 2*N - i + 1);
-            (L_1_copy, L_2_copy) = Troca(L_1_copy, L_2_copy, index, 2*N - i);
+            index = IndiceMaximo(L_1_copy, L_2_copy, 0, 2*N - i + 1);  // O(n)
+            (L_1_copy, L_2_copy) = Troca(L_1_copy, L_2_copy, index, 2*N - i);  // O(1)
         }
 
+        // Total: O(n²)
         return (L_1_copy, L_2_copy);
+    }
+}
+
+class Questao2
+{
+    private static int IndiceMinimo(List <int> L, int inicio, int fim)
+    {
+        List <int> L_copy = new List <int> (L);
+
+        int menor = L_copy[inicio], indiceMenor = inicio, N = L_copy.Count;
+
+        for (int i = inicio; i < fim; i += 2)
+        {
+            if (L[i] < menor)
+            {
+                menor = L[i];
+                indiceMenor = i;
+            }
+        }
+
+        return indiceMenor;
+    }
+
+    private static List <int> Trocar(List <int> L, int a, int b)
+    {
+        List <int> L_copy = new List <int> (L);
+
+        int temp = L_copy[a];
+        L_copy[a] = L_copy[b];
+        L_copy[b] = temp;
+
+        return L_copy;
+    }
+
+    public static List <int> OrdenaParidade(List <int> L)
+    {
+        var L_copy = new List <int> (L);
+        int index, N = L_copy.Count;
+
+        // O(n)
+        for (int i = 0; i < N; i++)
+        {
+            index = IndiceMinimo(L_copy, i, N);  // O(n)
+            L_copy = Trocar(L_copy, index, i);  // O(1)
+        }
+
+        // Total: O(n²) [não mudou nada da última questão pra essa]
+        return L_copy;
+    }
+}
+
+class Questao3
+{
+    // Idéia: Primeiro ordenamos a lista, depois achamos o pivô, vemos qual partição é menor, a que for, "refletimos" ela
+
+    private static int IndiceMaximo(List <int> L, int inicio, int fim)
+    {
+        List <int> L_copy = new List <int> (L);
+
+        int maior = L_copy[inicio], indiceMaior = inicio, N = L_copy.Count;
+
+        for (int i = inicio; i < fim; i++)
+        {
+            if (L_copy[i] > maior)
+            {
+                maior = L_copy[i];
+                indiceMaior = i;
+            }
+        }
+
+        return indiceMaior;
+    }
+
+    private static List <int> Trocar(List <int> L, int a, int b)
+    {
+        List <int> L_copy = new List <int> (L);
+
+        int temp = L_copy[a];
+        L_copy[a] = L_copy[b];
+        L_copy[b] = temp;
+
+        return L_copy;
+    }
+
+    private static List <int> SelectionSort(List <int> L, int a, int b, bool crescente)
+    {
+        var L_copy = new List <int> (L);
+        int index, N = L_copy.Count;
+
+        if (crescente == true)
+        {
+            for (int i = a; i < b; i++)
+            {
+                index = IndiceMaximo(L_copy, a, b - (i - a));
+                L_copy = Trocar(L_copy, index, b - (i - a) - 1);
+            }
+        }
+        else
+        {
+            for (int i = a; i < b; i++)
+            {
+                index = IndiceMaximo(L_copy, i, b);
+                L_copy = Trocar(L_copy, index, i);
+            }
+        }
+
+        return L_copy;
+    }
+
+    public static List <int> OrdenaUnimodal(List <int> L, int k)
+    {
+        var L_copy = new List <int> (L);
+	    int N = L.Count;
+
+	    L_copy = SelectionSort(L_copy, 0, N, true);  // O(n²)
+
+        if (N - k > k)
+        {
+            L_copy = SelectionSort(L_copy, k, N, false);  // O(n²)
+        }
+        else
+        {
+            L_copy = SelectionSort(L_copy, 0, k, false);  // O(n²)
+        }
+
+        // Total: O(n²)
+        return L_copy;
     }
 }
 
@@ -96,16 +236,16 @@ class Testes
 {
     public static void Main(String[] args)
     {
-        var a = new List <int> {1, 8, 24, 2};
-        var b = new List <int> {5, 3, 11, 9};
-        (a, b) = Questao1.SelectionSortDuplo(a, b);
+        var a = new List <int> {22, 28, 22, 28, 50, 39, 44, 3, 8, 33, 45, 16, 12, 3, 19, 16, 48, 46, 12, 9, 27, 50, 34, 41, 40, 21, 27, 10, 35, 14};
+        a = Questao3.OrdenaUnimodal(a, 18);
+        
         foreach (int element in a)
         {
-           Console.WriteLine(element);
-        }
-        foreach (int element in b)
-        {
-            Console.WriteLine(element);
+            for (int i = 0; i < element; i++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("");
         }
     }
 }
